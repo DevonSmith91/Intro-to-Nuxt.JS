7600042dd45e2d067525aedaaad6e2ce6e4d09d5
Devon <devonsmith91@gmail.com>
2020-07-28 13:26:44 -0400

    quick practice to learn Vue.js/Nuxt.js

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="single-recipe">
+    <h1>{{ recipe.title }}</h1>
+    <div>
+      <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title" />
+    </div>
+    <p>{{ recipe.descriptionText }}.</p>
+  </section>
+</template>
+
+<script>
+export default {
+    //context is specific to asyncData(?)
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          //real world, you can do a fetch for the specific restaurant that would be pulled into here.
+          //for this example we just used the same hard coded recipe list and will do a .filter on it to find what we are looking for based on the id
+          recipe: [
+            {
+              id: "1",
+              title: "Delicious Pizza",
+              previewText: "Awesome Pizza",
+              descriptionText: "This is really a delicious meal!",
+              thumbnail:
+                "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg",
+            },
+            {
+              id: "2",
+              title: "Tasty Food",
+              previewText: "Food so good",
+              descriptionText: "This is really a delicious meal!",
+              thumbnail:
+                "https://hips.hearstapps.com/del.h-cdn.co/assets/18/06/1517928338-delish-mongolian-ramen-and-meatballs-still001.jpg",
+            },
+          ].find(el => el.id === context.params.id),
+        });
+      }, 1500);
+    });
+  },
+};
+</script>
+
+<style scoped>
+.recipe-image{
+    width: 100%
+}
+
+.single-recipe {
+  display: flex;
+  justify-content: center;
+  flex-flow: column;
+  align-items: center;
+  text-align: center;
+  padding: 30px;
+}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <section class="recipes">
+        <Recipe 
+            v-for="recipe in recipes"
+            :key="recipe.id"
+            :thumbnail="recipe.thumbnail"
+            :title="recipe.title"
+            :id="recipe.id"
+            :previewText="recipe.previewText"
+        />
+    </section>
+</template>
+
+
+<script>
+import Recipe from '@/components/Recipe'
+
+export default {
+    components: {
+        Recipe
+    },
+    asyncData() {
+            // real life use, do a fetch here to a DB and return a promise that can be used to populate the data that will be put into these recipes
+            // return axios.get()
+            return new Promise((resolve, reject)=>{
+                setTimeout(()=>{
+                    resolve({
+                        recipes: [
+                            {
+                                id: "1",
+                                title: "Delicious Pizza",
+                                previewText: "Awesome Pizza",
+                                thumbnail: "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2016/11/one-pot-paneer-curry-pie.jpg"
+                            },
+                            {
+                                id: "2",
+                                title: "Tasty Food",
+                                previewText: "Food so good",
+                                thumbnail: "https://hips.hearstapps.com/del.h-cdn.co/assets/18/06/1517928338-delish-mongolian-ramen-and-meatballs-still001.jpg"
+                            }
+                        ]
+                    })
+                })
+            })
+    }
+};
+</script>
+
+
+<style scoped>
+.recipes {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+}
+</style>
